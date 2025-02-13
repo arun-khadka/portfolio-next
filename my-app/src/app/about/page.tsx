765d99f523ee0a0223000369686295f0d8f1a652
arunKhadka <arunkhadka009@gmail.com>
2025-02-13 21:03:46 +0545

    Dependency checked

--- my-app/src/app/about/page.tsx
+++ my-app/src/app/about/page.tsx
@@ -3,9 +3,11 @@ import AnimatedSection from "@/app/components/AnimatedSection";
 
 export default function AboutPage() {
   return (
-    <AnimatedSection>
-      <h2>About Me</h2>
-      <p>My story...</p>
-    </AnimatedSection>
+    <>
+      <AnimatedSection>
+        <h2>About Me</h2>
+        <p>My story...</p>
+      </AnimatedSection>
+    </>
   );
 }
